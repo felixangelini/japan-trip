a6f96f2f7a2a27eaa6111733e026d34be28f8015
Felix <felice.angelini94@gmail.com>
2025-07-29 17:34:05 +0200

    added manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
@@ -11,7 +11,19 @@ export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
   title: "Next.js and Supabase Starter Kit",
   description: "The fastest way to build apps with Next.js and Supabase",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: "Japan Trip",
+  },
 };
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +38,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      <meta name="apple-mobile-web-app-title" content="Japan Trip" />
+      <link rel="apple-touch-icon" href="/192.png" />
+      <link rel="apple-touch-startup-image" href="/512.png" />
+      {/* OG IMAGE */}
+      <meta property="og:title" content={metadata.title as string} />
+      <meta property="og:description" content={metadata.description as string} />
+      <meta property="og:image" content="/512.png" />
+      <meta property="og:url" content="https://amatelini-japan-trip.vercel.app/" />
+      <meta property="og:type" content="website" />
       <body className={`${geistSans.className} antialiased`}>
         <ThemeProvider
           attribute="class"
---
+++ app/manifest.ts
@@ -0,0 +1,47 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Japan Trip',
+    short_name: 'Japan Trip',
+    description: 'Our Japan Trip',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#000000',
+    theme_color: '#000000',
+    icons: [
+      {
+        src: '/144.png',
+        sizes: '144x144',
+        type: 'image/png',
+        purpose: 'any'
+      },
+      {
+        src: '/192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable'
+      },
+      {
+        src: '/512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable'
+      }
+    ],
+    screenshots: [
+      {
+        src: '/512.png',
+        type: 'image/png',
+        sizes: '512x512',
+        form_factor: 'narrow'
+      },
+      {
+        src: '/512.png',
+        type: 'image/png',
+        sizes: '512x512',
+        form_factor: 'wide'
+      }
+    ]
+  }
+}
